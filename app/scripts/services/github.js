c81de6329d99098bb34e49df50413f8abacb65b9
Roger Hutchings <roger@hutchin.gs>
2014-09-27 15:44:48 +0200

    Add basic github service

--- app/scripts/services/github.js
+++ app/scripts/services/github.js
@@ -8,12 +8,44 @@
  * Factory in the githubBatchApp.
  */
 angular.module('githubBatchApp')
-    .factory('Github', function () {
+    .factory('Github', function ($http, OAuth) {
 
+        // TODO: Replace with interceptor
+        var appendToken = function (url) {
+            return url + '?access_token=' + OAuth.token;
+        };
 
+        var getRepos = function () {
 
+            // TODO: Get user repos
+            // var orgs = $http({
+            //     method: 'GET',
+            //     url: appendToken('https://api.github.com/user/orgs'),
+            //     cache: true
+            // });
+
+            // Replace with promise.all when getting org repos
+            return $http({
+                method: 'GET',
+                url: appendToken('https://api.github.com/user/repos'),
+                cache: true
+            });
+        };
+
+        var getUser = function () {
+            return $http({
+                method: 'GET',
+                url: appendToken('https://api.github.com/user'),
+                cache: true
+            });
+        };
+
+        var submitIssues = function (args) {
+        };
 
         return {
-            
+            getRepos: getRepos,
+            getUser: getUser,
+            submitIssues: submitIssues
         };
     });
