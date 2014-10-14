28707b2bf6c1842a80303a425a89cb4d7deda53a
Roger Hutchings <roger@hutchin.gs>
2014-10-14 19:11:40 +0100

    Make tests run continuously

--- web/Gruntfile.js
+++ web/Gruntfile.js
@@ -387,7 +387,8 @@ module.exports = function (grunt) {
         karma: {
             unit: {
                 configFile: 'test/karma.conf.js',
-                singleRun: true
+                singleRun: false,
+                autoWatch: true
             }
         }
     });
