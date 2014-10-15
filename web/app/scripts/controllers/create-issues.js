79cc77ad6aad697aa0bbd6a2e6d58cdd96c25fdd
Roger Hutchings <roger@hutchin.gs>
2014-10-15 12:29:06 +0100

    Wrap in IIFE

--- web/app/scripts/controllers/create-issues.js
+++ web/app/scripts/controllers/create-issues.js
@@ -1,34 +1,43 @@
-'use strict';
+(function (angular, _) {
 
-/**
- * @ngdoc function
- * @name githubBatchApp.controller:CreateIssuesCtrl
- * @description
- * # CreateIssuesCtrl
- * Controller of the githubBatchApp
- */
-angular.module('githubBatchApp')
-    .controller('CreateIssuesCtrl', function ($scope, $location, Github, Issues, OAuth) {
-        
-        if (!OAuth.token) {
-            $location.path('/')
-        }
+    'use strict';
 
-        $scope.github = Github; // neccesary?
-        $scope.issues = Issues;
-
-        $scope.addIssue = function () {
-            $scope.issues.createIssue();
-        };
-
-        $scope.deleteIssue = function (index) {
-            $scope.issues.deleteIssue(index);
-        };
-
-        $scope.checkTitle = function (title) {
-            if (_.isUndefined(title)) {
-                return 'Issue title cannot be blank.';
+    /**
+     * @ngdoc function
+     * @name githubBatchApp.controller:CreateIssuesCtrl
+     * @description
+     * # CreateIssuesCtrl
+     * Controller of the githubBatchApp
+     */
+    angular.module('githubBatchApp')
+        .controller('CreateIssuesCtrl', [
+            '$scope',
+            '$location', 
+            'Issues', 
+            'OAuth',
+            function ($scope, $location, Issues, OAuth) {
+            
+            if (!OAuth.token) {
+                $location.path('/');
             }
-        };
 
-    });
+            $scope.issues = Issues;
+
+            $scope.addIssue = function () {
+                $scope.issues.createIssue();
+            };
+
+            $scope.deleteIssue = function (index) {
+                $scope.issues.deleteIssue(index);
+            };
+
+            $scope.checkTitle = function (title) {
+                if (_.isUndefined(title)) {
+                    return 'Issue title cannot be blank.';
+                } 
+                return true;
+            };
+
+        }]);
+
+})(window.angular, window._);
