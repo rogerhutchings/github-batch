a8e836cc4b555421dd3a456de9450c6c9e4cdf47
Roger Hutchings <roger@hutchin.gs>
2014-10-20 10:13:47 +0100

    Refactor GETs

--- web/app/scripts/services/github.js
+++ web/app/scripts/services/github.js
@@ -23,30 +23,23 @@
                 var _defaults = {
                     headers: {
                         Authorization: 'token ' + OAuth.token
-                    }
+                    },
+                    cache: true
                 };
 
                 // Return a promise containing a list of repos the user has 
                 // access to.
                 // TODO: Ensure that we also get org repos
                 var getRepos = function () {
-
-                    return $http(_.extend(_defaults, {
-                        method: 'GET',
-                        url: _endpoint('/user/repos'),
-                        cache: true
-                    })).then(function (response) {
-                        return response.data;
-                    });
+                    return $http.get(_endpoint('/user/repos'), _defaults)
+                        .then(function (response) {
+                            return response.data;
+                        });
                 };
 
                 // Return a promise with the user information
                 var getUser = function () {
-                    return $http(_.extend(_defaults, {
-                        method: 'GET',
-                        url: _endpoint('/user'),
-                        cache: true
-                    }));
+                    return $http.get(_endpoint('/user'), _defaults);
                 };
 
                 // Submit the issues to Github. Since issues can only be created
@@ -60,6 +53,7 @@
                         promises.push($http(_.extend(_defaults, {
                             method: 'POST',
                             url: args.repo + '/issues',
+                            cache: false,
                             data: {
                                 title: issue.title,
                                 body: issue.body
